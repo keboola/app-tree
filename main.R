db7457d01c5c2b14aace1145ba91a3a6f1bbb413
Ondrej Popelka <odinuv@gmail.com>
2017-02-07 23:15:46 +0100

    feat: return also root node title

--- main.R
+++ main.R
@@ -9,10 +9,22 @@ getLevel <- function(data, id, idColumn, parentIdColumn) {
   }
   level
 }
+getRoot <- function(data, id, idColumn, parentIdColumn) {  
+  parent <- data[which(data[[idColumn]] == id),][[parentIdColumn]]
+  if (parent > 0) {
+    root <- getRoot(data, parent, idColumn, parentIdColumn);
+  } else {
+    root <- data[which(data[[idColumn]] == id),][['title']];
+  }
+  root
+}
 
 outData <- data
 outData$levels <- sapply(data$categoryId, function(val) {
   getLevel(data, val, 'categoryId', 'categoryParentId')
+})
+outData$root <- sapply(data$categoryId, function(val) {
+  getRoot(data, val, 'categoryId', 'categoryParentId')
 })
 
 write.csv(outData, file = "/data/out/tables/tree.csv", row.names = FALSE)
