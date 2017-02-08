99e1760606ec5f32874049241b4d59e0611d7b07
Ondrej Popelka <odinuv@gmail.com>
2017-02-08 10:26:11 +0100

    fix: use proper entrypoint

--- main.R
+++ main.R
@@ -1,6 +1,17 @@
-suppressMessages(library('keboola.r.custom.application.tree', quiet = TRUE))
+#suppressMessages(library('keboola.r.custom.application.tree', quiet = TRUE))
+install.packages('jsonlite', repos = c("https://cran.r-project.org"))
+library('devtools')
+library('methods')
+library('jsonlite')
+
+# install the transformation application ancestors
+devtools::install_github('keboola/r-application', ref = "master", force = TRUE)
+devtools::install_github('keboola/r-docker-application', ref = "master", force = TRUE)
+library('keboola.r.application')
+library('keboola.r.docker.application')
+source('R/keboola.r.custom.application.tree.R')
 
 # run it
-app <- RTree$new()
+app <- RTree$new('/data/')
 ret <- app$readConfig()
 ret <- app$run()
