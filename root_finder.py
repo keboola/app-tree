39f31d05de2923919a0971bed37bd8ede1702859
Tomas Kacur <tomas.kacur@keboola.com>
2017-12-11 18:41:12 +0100

    wip levels

--- root_finder.py
+++ root_finder.py
@@ -1,20 +1,32 @@
 import csv
 from keboola import docker
 
-def update_parents(parent, child, roots_map, nodes_map):
+def update_parents(parent, child, roots_map, nodes_map, levels_map, parent_level):
+  print("update for", child, parent)
   if child in nodes_map:
+    if parent in nodes_map:
+      newLevel = parent_level
+    else:
+      newLevel = levels_map.get(child, 1) + 1
+    levels_map[child] = newLevel
     children = nodes_map.pop(child)
     for c in children:
       roots_map[c] = parent
+      levels_map[c] = levels_map.get(c, 0) + 1
     children.add(child)
     parentChildren = nodes_map.get(parent, set())
     parentChildren.update(children)
     nodes_map[parent] = parentChildren
   else:
     newChildren = nodes_map.get(parent, set())
+    newLevel = parent_level
+    # if any(newChildren):
+    #   newLevel = levels_map[min(newChildren)]
+    print("newLEvel", child, parent, newLevel, newChildren)
     newChildren.add(child)
     nodes_map[parent] = newChildren
-  return (roots_map, nodes_map)
+    levels_map[child] = newLevel
+  return (roots_map, nodes_map, levels_map)
 
 
 def find_roots(rows, parent_column, child_column):
@@ -22,6 +34,7 @@ def find_roots(rows, parent_column, child_column):
   nodes_map = {}
   # child - parent pais map
   roots_map = {}
+  levels_map = {}
   null_nodes = set()
   for row in rows:
     parent = row[parent_column]
@@ -29,23 +42,34 @@ def find_roots(rows, parent_column, child_column):
     if parent == '0' or parent == '':
       nodes_map[child] = set()
       null_nodes.add(child)
+      levels_map[child] = 1
     else:
       new_parent = parent
       if parent in roots_map:
         new_parent = roots_map[parent]
+      parent_level = levels_map.get(parent, levels_map.setdefault(parent, 1)) + 1
       roots_map[child] = new_parent
-      roots_map, nodes_map = update_parents(new_parent, child, roots_map, nodes_map)
+      for c in nodes_map.get(child, set()):
+        levels_map[c] = parent_level
+        for cc in nodes_map.get(c, set()):
+          levels_map[cc] = parent_level + 1
+      roots_map, nodes_map, levels_map = update_parents(new_parent, child, roots_map, nodes_map, levels_map, parent_level)
+
+    print("ROW", row)
+    print("levels", levels_map)
+    print("roots", roots_map)
+    print("nodes map", nodes_map)
 
     #print(row, roots_map, nodes_map)
   #print("roots map", roots_map)
-  #print("nodes map", nodes_map)
-  print("DONE:", "Number of roots found:", len(null_nodes), "Number of nodes with root found:", len(roots_map))
+
+  print("DONE:", "Number of roots found:", len(null_nodes), "Number of nodes with root found:", len(roots_map), "levels", levels_map)
   return {"roots": roots_map, "null_nodes": null_nodes}
 
 
 
 def run():
-  cfg = docker.Config()
+  cfg = docker.Config('tmp')
   parameters = cfg.get_parameters()
   c_parent = parameters.get('parent_column')
   c_child = parameters.get('child_column')
